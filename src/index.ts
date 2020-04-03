408416fde9b7341c356e51f23ae81cb0567cc5e5
taoqf <tao_qiufeng@126.com>
2020-04-03 12:57:42 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import awx2 from '@mmstudio/awx000002';
 
-export default function awx23(param1: string, param2: string) {
+export default function get<T>(mm: awx2, key: string, default_value?: T) {
+	const local = mm.data.local as { [key: string]: unknown; };
+	return (local[key] as T) || default_value;
 }
